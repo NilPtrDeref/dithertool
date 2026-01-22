7978d93ac673c4e747b70d24aa06a41529349aef
James Wood <jwooddev@gmail.com>
2026-01-21 22:56:31 -0500

    Add png signature check

--- src/image/png.zig
+++ src/image/png.zig
@@ -2,6 +2,10 @@ const std = @import("std");
 const Io = std.Io;
 const Allocator = std.mem.Allocator;
 
+const PngError = error{
+    InvalidSignature,
+};
+
 const Self = @This();
 io: Io,
 gpa: Allocator,
@@ -17,6 +21,12 @@ pub fn parse(io: Io, gpa: Allocator, reader: *Io.Reader) !*Self {
     self.reader = reader;
     self.width = 0;
     self.height = 0;
+
+    // Check PNG signature
+    const signature: u64 = std.mem.nativeToBig(u64, 0x89504E470D0A1A0A);
+    if (!std.mem.eql(u8, try reader.peek(8), std.mem.asBytes(&signature))) return PngError.InvalidSignature;
+    reader.take(8);
+
     return self;
 }
 
