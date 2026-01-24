75ae92d9587e8be45403eff906d6d2fd3b44cf4c
James Wood <jwooddev@gmail.com>
2026-01-23 21:00:20 -0500

    Check palette chunks

--- src/image/png.zig
+++ src/image/png.zig
@@ -4,9 +4,16 @@ const Allocator = std.mem.Allocator;
 
 const PngError = error{
     InvalidSignature,
+    InvalidChunkType,
     InvalidHeaderLength,
+    InvalidColorType,
+    InvalidBitDepth,
     InvalidCompressionMethod,
     InvalidFilterMethod,
+    InvalidInterlaceMethod,
+    InvalidPaletteSize,
+    TooManyPalettes,
+    MissingPalette,
 };
 
 const PngSignature: u64 = 0x89504E470D0A1A0A;
@@ -38,6 +45,7 @@ const ChunkType = enum(u32) {
     acTL = 0x6163544C,
     fcTL = 0x6663544C,
     fdAT = 0x66644154,
+    _,
 };
 
 const ColorType = enum(u8) {
@@ -46,12 +54,22 @@ const ColorType = enum(u8) {
     Indexed = 3,
     GreyscaleAlpha = 4,
     TruecolorAlpha = 6,
+    _,
 };
 
 const InterlaceMethod = enum(u8) {
     None = 0,
     Adam7 = 1,
+    _,
 };
+
+const RGB = struct {
+    r: u8,
+    g: u8,
+    b: u8,
+};
+
+const Palette = std.ArrayList(RGB);
 
 const Self = @This();
 io: Io,
@@ -62,6 +80,7 @@ height: u32,
 bitdepth: u8,
 colortype: ColorType,
 interlace: InterlaceMethod,
+palette: ?Palette,
 
 pub fn parse(io: Io, gpa: Allocator, reader: *Io.Reader) !*Self {
     var self = try gpa.create(Self);
@@ -94,36 +113,82 @@ fn parse_chunk(self: *Self) !void {
 
     std.log.info("{any}", .{chunktype});
     switch (chunktype) {
+        _ => return PngError.InvalidChunkType,
         .IHDR => {
-            try self.parse_IHDR(chunklen);
+            if (chunklen != 13) return PngError.InvalidHeaderLength;
+            self.width = try self.reader.takeInt(u32, .big);
+            self.height = try self.reader.takeInt(u32, .big);
+
+            self.bitdepth = try self.reader.takeByte();
+            self.colortype = @enumFromInt(try self.reader.takeByte());
+            switch (self.colortype) {
+                .Greyscale => switch (self.bitdepth) {
+                    1, 2, 4, 8, 16 => {},
+                    else => return PngError.InvalidBitDepth,
+                },
+                .Truecolor => switch (self.bitdepth) {
+                    8, 16 => {},
+                    else => return PngError.InvalidBitDepth,
+                },
+                .Indexed => switch (self.bitdepth) {
+                    1, 2, 4, 8 => {},
+                    else => return PngError.InvalidBitDepth,
+                },
+                .GreyscaleAlpha => switch (self.bitdepth) {
+                    8, 16 => {},
+                    else => return PngError.InvalidBitDepth,
+                },
+                .TruecolorAlpha => switch (self.bitdepth) {
+                    8, 16 => {},
+                    else => return PngError.InvalidBitDepth,
+                },
+                _ => return PngError.InvalidColorType,
+            }
+
+            if (try self.reader.takeByte() != 0) return PngError.InvalidCompressionMethod;
+            if (try self.reader.takeByte() != 0) return PngError.InvalidFilterMethod;
+            self.interlace = @enumFromInt(try self.reader.takeByte());
+            switch (self.interlace) {
+                _ => return PngError.InvalidInterlaceMethod,
+                else => {},
+            }
+
+            _ = try self.reader.takeInt(u32, .big); // Discard CRC
         },
         .IEND => {
             _ = try self.reader.takeInt(u32, .big); // Discard CRC
         },
+        .PLTE => {
+            if (self.palette != null) return PngError.TooManyPalettes;
+            const palettesize = chunklen / 3;
+            switch (self.colortype) {
+                .Indexed => {
+                    if (palettesize > std.math.pow(u32, 2, self.bitdepth)) return PngError.InvalidPaletteSize;
+                },
+                .Truecolor, .TruecolorAlpha => {
+                    if (palettesize > 256) return PngError.InvalidPaletteSize;
+                },
+                else => return PngError.TooManyPalettes,
+            }
+            if (chunklen == 0 or @mod(chunklen, 3) != 0) return PngError.InvalidPaletteSize;
+
+            self.palette = try .initCapacity(self.gpa, palettesize);
+            for (0..palettesize) |_| {
+                self.palette.?.appendAssumeCapacity(.{
+                    .r = try self.reader.takeByte(),
+                    .g = try self.reader.takeByte(),
+                    .b = try self.reader.takeByte(),
+                });
+            }
+            _ = try self.reader.takeInt(u32, .big); // Discard CRC
+        },
         else => {
             _ = try self.reader.discard(.limited(chunklen));
-            const crc = try self.reader.takeInt(u32, .big);
-            _ = crc;
+            _ = try self.reader.takeInt(u32, .big);
         },
     }
-}
 
-fn parse_IHDR(self: *Self, len: u32) !void {
-    if (len != 13) return PngError.InvalidHeaderLength;
-
-    self.width = try self.reader.takeInt(u32, .big);
-    self.height = try self.reader.takeInt(u32, .big);
-
-    // TODO: Validate bitdepth against color type
-    self.bitdepth = try self.reader.takeByte();
-    self.colortype = @enumFromInt(try self.reader.takeByte());
-
-    if (try self.reader.takeByte() != 0) return PngError.InvalidCompressionMethod;
-    if (try self.reader.takeByte() != 0) return PngError.InvalidFilterMethod;
-
-    self.interlace = @enumFromInt(try self.reader.takeByte());
-
-    _ = try self.reader.takeInt(u32, .big); // Discard CRC
+    if (self.colortype == .Indexed and self.palette == null) return PngError.MissingPalette;
 }
 
 pub fn deinit(self: *Self) void {
